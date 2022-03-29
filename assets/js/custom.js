9e77fee32f2114d39af4adef14f61a7cc93f74b0
Marc Lacroix <mlacroix@updraftplus.com>
2022-03-29 16:43:21 +0200

    Initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+( function( $ ) {
+
+	wp.hooks.addAction( 'PC.fe.start', 'MKL/PC/Custom_Code', function( view ) {
+		// Execute some code when the configurator starts. 
+		// All the DOM has been added, and before showing it.
+		// A good place to move or add dom elements, unless if they are related to choices or layers. 
+		// In this case, do this using the appropriate hooks.
+	});
+
+} )( jQuery );
---
+++ configurator-custom-code.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Plugin Name:       Configurator - Custom code bootstrap
+ * Plugin URI:        http://wc-product-configurator.com/
+ * Description:       Use this plugin to start adding custom code to the configurator
+ * Version:           1.0
+ * Author:            marcusig
+ * Author URI:        https://mklacroix.com
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+	die;
+}
+
+add_action( 'mkl_pc_scripts_product_page_after', 'mkl_custom_add_scripts' );
+
+function mkl_custom_add_scripts() {
+	// List of dependencies
+	$dependencies = [
+		'jquery',
+		'wp-util',
+		'wp-hooks',
+		'mkl_pc/js/views/configurator'
+	];
+	// Enqueu a script
+	wp_enqueue_script( 
+		'mkl_pc/Custom/js', 
+		trailingslashit( plugin_dir_url( __FILE__ ) ) . 'assets/js/custom.js', 
+		$dependencies, 
+		filemtime( trailingslashit( plugin_dir_path ( __FILE__ ) ) . 'assets/js/custom.js' ), 
+		true
+	);
+
+	// Enqueu a CSS file
+	wp_enqueue_style(
+		'mkl_pc/Custom/js', 
+		trailingslashit( plugin_dir_url( __FILE__ ) ) . 'assets/css/custom.css', 
+		[],
+		filemtime( trailingslashit( plugin_dir_path ( __FILE__ ) ) . 'assets/css/custom.css' ), 
+	);
+}
+
+/**
+ * Force loading the configurator assets on the frontpage
+
+ add_filter( 'load_configurator_on_page', function( $load ) {
+	if ( is_front_page() ) return true;
+	return $load;
+} );
+
+// Force loading the configurator assets on every page
+// add_filter( 'load_configurator_on_page', '__return_true' );
+*/
+
